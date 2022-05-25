4b496cb1c34ca09c96d481a15b8f809f0080b283
TimD <timothee.diroian@gmail.com>
2022-05-25 11:05:47 +0200

    Add in the function the LineBreaker for the grid.

--- src/script.js
+++ src/script.js
@@ -5,6 +5,19 @@ function placeMines(rows, columns, mines) {
   var lenghtString = (rows * columns) + numLineBreak;
   // Creation of the base of the grid
   var grid = new Array(lenghtString).fill('_');
+  console.log(lenghtString)
+  // Defined a random function to place mine in the grid
+  const random = (lenghtString) => Math.floor(Math.random()*lenghtString);
+
+  // Placing the LineBreaker in our grid
+  for (let index = 0; index < numLineBreak; index++) {
+    var line = index+1;
+    var length = (columns*line) + index;
+    grid[length] = '\n';
+    //console.log(line)
+    //console.log(length)
+  };
+
   return grid.join('');
 }
 
@@ -20,4 +33,5 @@ const log = msg => {
 // Try uncommenting this
 // log(placeMines(2, 4, 3))
 
-// console.log(placeMines(2, 4, 3))
+ //console.log(placeMines(5, 5, 5))
+ //placeMines(3, 4, 3)
