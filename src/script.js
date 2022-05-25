aa250bb53f67a3fba5e63754d5a32a7afd2f2dca
TimD <timothee.diroian@gmail.com>
2022-05-25 11:55:05 +0200

    Adding the placing mine randomly logic with test passed

--- src/script.js
+++ src/script.js
@@ -1,25 +1,34 @@
 // Finish the program
 function placeMines(rows, columns, mines) {
   var numLineBreak = rows - 1;
-  var numMines = mines;
   var lenghtString = (rows * columns) + numLineBreak;
   // Creation of the base of the grid
   var grid = new Array(lenghtString).fill('_');
-  console.log(lenghtString)
+
   // Defined a random function to place mine in the grid
-  const random = (lenghtString) => Math.floor(Math.random()*lenghtString);
+  const random = () => Math.floor(Math.random()*lenghtString);
 
   // Placing the LineBreaker in our grid
-  for (let index = 0; index < numLineBreak; index++) {
-    var line = index+1;
-    var length = (columns*line) + index;
-    grid[length] = '\n';
-    //console.log(line)
-    //console.log(length)
+  for (let i = 0; i < numLineBreak; i++) {
+    var line = i+1;
+    // We want to catch every end of line by multiplying the length of colum + the letter to add the breakline caractere
+    var endLine = (columns*line) + i;
+    grid[endLine] = '\n';
+  };
+
+  // Placing the mine randomly
+  while(0 < mines){
+    // Storing an random number inside the placeMine
+    var placeMine = random();
+    // Check if it will not have a conflict with a break line or an already existing mine
+    if(grid[placeMine] !== '\n' && grid[placeMine] !== 'x'){
+      mines--;
+      grid[placeMine] = 'x';
+    };
   };
 
   return grid.join('');
-}
+};
 
 // export the function to the test unit
 module.exports = placeMines;
@@ -33,5 +42,5 @@ const log = msg => {
 // Try uncommenting this
 // log(placeMines(2, 4, 3))
 
- //console.log(placeMines(5, 5, 5))
+ //console.log(placeMines(10, 10, 10))
  //placeMines(3, 4, 3)
